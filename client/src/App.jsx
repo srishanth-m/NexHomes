f79f35e7d5dba7e1a43919a29356fec50ad47a3c
srihanth-m <srishanthmule@gmail.com>
2024-03-12 07:55:53 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
+import Profile from '../src/pages/Profile'
+import SignIn from '../src/pages/SignIn'
+import SignOut from '../src/pages/SignOut'
 
 export default function App() {
   return (
-    <div className='bg-slate-200'>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/signIn" element={<SignIn/>}></Route>
+        <Route path="/signOut" element={<SignOut/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
