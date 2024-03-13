9b309872de7ed325c98f95eac2f25ccc374950df
srihanth-m <srishanthmule@gmail.com>
2024-03-13 08:11:01 +0530

    connected server to the database but couldnt use env variable

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose.connect("mongodb+srv://srishanth:srishanth_m@cluster0.fojwfwp.mongodb.net/NexHomes?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => { console.log("connected to mongodb") })
+    .catch((err) => console.log(err))
 
 const app = express()
 
-app.listen(3000 , () =>{
+app.listen(3000, () => {
     console.log("app is listening at port 3000")
 })
