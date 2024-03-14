cf64ea64bbfb2078d53284a96cc2cff72b529af9
srihanth-m <srishanthmule@gmail.com>
2024-03-14 13:34:39 +0530

    created signup api route and a middleware function to handle errors

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
+
+export const signUp = async (req, res , next) => {
+    const { username, email, password } = req.body
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword })
+    try {
+        await newUser.save()
+        res.status(201).json("user created and saved successfully")
+
+    } catch (err) {
+        next(err)
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config()
 
 mongoose.connect("mongodb+srv://srishanth:srishanth_m@cluster0.fojwfwp.mongodb.net/NexHomes?retryWrites=true&w=majority&appName=Cluster0")
@@ -8,7 +10,21 @@ mongoose.connect("mongodb+srv://srishanth:srishanth_m@cluster0.fojwfwp.mongodb.n
     .catch((err) => console.log(err))
 
 const app = express()
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("app is listening at port 3000")
+})
+
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "internal server error"
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
 })
