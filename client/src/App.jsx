a72c249368a8426b251c9eb510327eea842f5ee4
srihanth-m <srishanthmule@gmail.com>
2024-03-19 22:55:04 +0530

    created profile page ui and its access

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from '../src/pages/Home'
-import About from '../src/pages/About'
-import Profile from '../src/pages/Profile'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import Profile from "../src/pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
-        <Route path="/signUp" element={<SignUp/>}></Route>
-        <Route path="/signIn" element={<SignIn/>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/signUp" element={<SignUp />}></Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
